37ee06e5d9119c3d230289b07702c50c5acde465
kgdmKZ <andrew.zadravec@gmail.com>
2017-09-03 14:01:56 -0400

    finished council district service and added repository methods

--- Baltimore Neighborhood Data/src/main/java/baltimoredata/repository/CouncilDistrictRepository.java
+++ Baltimore Neighborhood Data/src/main/java/baltimoredata/repository/CouncilDistrictRepository.java
@@ -1,9 +1,11 @@
 package baltimoredata.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import baltimoredata.model.CouncilDistrict;
 
-public interface CouncilDistrictRepository extends CrudRepository<CouncilDistrict, Integer> {
+public interface CouncilDistrictRepository extends PagingAndSortingRepository<CouncilDistrict, Integer> {
     CouncilDistrict findByDistrictNumber(Integer districtNumber);
+	Long removeById(Integer id);
+	Long removeByDistrictNumber(Integer districtNumber);
 }
---
+++ Baltimore Neighborhood Data/src/main/java/baltimoredata/service/impl/CouncilDistrictServiceImpl.java
@@ -0,0 +1,87 @@
+package baltimoredata.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import baltimoredata.exception.ConflictException;
+import baltimoredata.exception.ResourceNotFoundException;
+import baltimoredata.model.CouncilDistrict;
+import baltimoredata.repository.CouncilDistrictRepository;
+
+@Service
+@Transactional
+public class CouncilDistrictServiceImpl {
+	@Autowired
+    CouncilDistrictRepository councilRepository;
+    
+    @Transactional(readOnly=true)
+	public Long getCouncilDistrictCount() {
+		return councilRepository.count();
+	}
+	
+	@Transactional(readOnly=true)
+	public Page<CouncilDistrict> getCouncilDistricts(Pageable pageReq) {
+		return councilRepository.findAll(pageReq);
+	}
+	
+	@Transactional(readOnly=true)
+	public CouncilDistrict getCouncilDistrictById(Integer id) {
+		return councilRepository.findOne(id);
+	}
+	
+	@Transactional(readOnly=true)
+	public CouncilDistrict getCouncilDistrictByDistrictNumber(Integer districtNumber) {
+		return councilRepository.findByDistrictNumber(districtNumber);
+	}
+	
+	public CouncilDistrict addCouncilDistrict(CouncilDistrict c) {
+		Integer districtNumber = c.getDistrictNumber();
+		CouncilDistrict res = councilRepository.findByDistrictNumber(districtNumber);
+		if (res != null) {
+			throw new ConflictException("council district", "districtNumber", districtNumber.toString());
+		}
+		return councilRepository.save(c);
+	}
+	
+	private void modifyCouncilDistrict(CouncilDistrict modified, CouncilDistrict existing) {
+		Integer newDistrictNumber = modified.getDistrictNumber();
+		CouncilDistrict conflictCouncilDistrict = councilRepository.findByDistrictNumber(newDistrictNumber);
+		Integer existingId = existing.getId();
+		Integer conflictId = conflictCouncilDistrict == null ? null : conflictCouncilDistrict.getId();
+		
+		if (conflictId != null && conflictId != existingId) {
+			throw new ConflictException("council district", "districtNumber", newDistrictNumber.toString());
+		}
+		
+		existing.setDistrictNumber(newDistrictNumber);
+		councilRepository.save(existing);
+	}
+	
+	public void modifyCouncilDistrictById(Integer id, CouncilDistrict modified) {
+		CouncilDistrict existing = councilRepository.findOne(id);
+		if (existing == null) {
+			throw new ResourceNotFoundException("council district", "id", id.toString());
+		}
+		modifyCouncilDistrict(modified, existing);
+	}
+	 
+	public void modifyCouncilDistrictByDistrictNumber(Integer districtNumber, CouncilDistrict modified) {
+		CouncilDistrict existing = councilRepository.findByDistrictNumber(districtNumber);
+		if (existing == null) {
+			throw new ResourceNotFoundException("council district", "districtNumber", districtNumber.toString());
+		}
+		modifyCouncilDistrict(modified, existing);
+	}
+	
+	public Long deleteCouncilDistrictById(Integer id) {
+		return councilRepository.removeById(id);
+	}
+	
+	public Long deleteCouncilDistrictByDistrictNumber(Integer districtNumber) {
+		return councilRepository.removeByDistrictNumber(districtNumber);
+	}
+
+}
