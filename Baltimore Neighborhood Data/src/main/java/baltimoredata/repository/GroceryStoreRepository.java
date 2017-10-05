8b4c17f055126445c2e47f21926b249599c7731e
kgdmKZ <andrew.zadravec@gmail.com>
2017-10-05 15:48:11 -0400

    Added controllers and services for library and grocery store POIs

--- Baltimore Neighborhood Data/src/main/java/baltimoredata/repository/GroceryStoreRepository.java
+++ Baltimore Neighborhood Data/src/main/java/baltimoredata/repository/GroceryStoreRepository.java
@@ -1,9 +1,15 @@
 package baltimoredata.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import baltimoredata.model.GroceryStore;
 
-public interface GroceryStoreRepository extends CrudRepository<GroceryStore, Integer> {
-
+public interface GroceryStoreRepository extends PagingAndSortingRepository<GroceryStore, Integer> {
+	GroceryStore findByAddress_Id(Integer addressId);
+	GroceryStore findByAddress_Neighborhood_IdAndAddress_StreetAddress(Integer neighborhoodId, String streetAddress);
+	GroceryStore findByAddress_Neighborhood_NameAndAddress_StreetAddress(String neighborhoodName, String streetAddress);
+	Long removeById(Integer id);
+	Long removeByAddress_Id(Integer addressId);
+	Long removeByAddress_Neighborhood_IdAndAddress_StreetAddress(Integer neighborhoodId, String streetAddress);
+	Long removeByAddress_Neighborhood_NameAndAddress_StreetAddress(String neighborhoodName, String streetAddress);
 }
--- Baltimore Neighborhood Data/src/main/java/baltimoredata/repository/LibraryRepository.java
+++ Baltimore Neighborhood Data/src/main/java/baltimoredata/repository/LibraryRepository.java
@@ -1,9 +1,15 @@
 package baltimoredata.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import baltimoredata.model.Library;
 
-public interface LibraryRepository extends CrudRepository<Library, Integer> {
-
+public interface LibraryRepository extends PagingAndSortingRepository<Library, Integer> {
+	Library findByAddress_Id(Integer addressId);
+	Library findByAddress_Neighborhood_IdAndAddress_StreetAddress(Integer neighborhoodId, String streetAddress);
+	Library findByAddress_Neighborhood_NameAndAddress_StreetAddress(String neighborhoodName, String streetAddress);
+	Long removeById(Integer id);
+	Long removeByAddress_Id(Integer addressId);
+	Long removeByAddress_Neighborhood_IdAndAddress_StreetAddress(Integer neighborhoodId, String streetAddress);
+	Long removeByAddress_Neighborhood_NameAndAddress_StreetAddress(String neighborhoodName, String streetAddress);
 }
